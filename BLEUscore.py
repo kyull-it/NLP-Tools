c16e95910177e8676257506460568137d45444fa
Yun Gyu Ri <67450333+kyull-it@users.noreply.github.com>
2023-06-01 16:19:12 +0900

    Add files via upload

--- BLEUscore.py
+++ BLEUscore.py
@@ -20,7 +20,7 @@ print(args.file)
 
 # 데이터 로드, 컬럼명 지정
 df = pd.read_excel(args.file, header=0)
-df.columns = ['ko_original', 'matis']
+df.columns = ['ko_original', 'result']
 
 # bleu_score, Smoothing Function
 cc = SmoothingFunction()
@@ -30,7 +30,7 @@ mecab = Mecab()
 
 
 reference = [mecab.morphs(df['ko_original'][0])]
-target_text = mecab.morphs(df['matis'][0])
+target_text = mecab.morphs(df['result'][0])
 score = sentence_bleu(reference, target_text, weights=[1,0,0,0], smoothing_function=cc.method2, auto_reweigh=True)
 
 print("original sentence : ", reference)
